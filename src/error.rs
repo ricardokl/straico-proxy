f7e6977f15b50e533a0ee4e12b5d940d3d3c65a6
Ricardo Kullock <ricardokullock@gmail.com>
2025-09-15 17:56:35 +0200

    fix: handle straico client error

--- src/error.rs
+++ src/error.rs
@@ -3,6 +3,7 @@ use serde_json::Value;
 use std::fmt::Debug;
 use reqwest::Error as ReqwestError;
 use thiserror::Error;
+use straico_client::error::Error as StraicoError;
 
 use crate::streaming::create_error_chunk;
 use anyhow::Error as AnyhowError;
@@ -13,6 +14,8 @@ pub enum CustomError {
     SerdeJson(#[from] serde_json::Error),
     #[error("Error from HTTP client")]
     ReqwestClient(#[from] ReqwestError),
+    #[error("Error from Straico API")]
+    Straico(#[from] StraicoError),
     #[error("Failed to parse response from Straico API")]
     ResponseParse(Value),
     #[error("An internal error occurred")]
@@ -30,6 +33,7 @@ impl ResponseError for CustomError {
         match *self {
             CustomError::SerdeJson(_) => StatusCode::BAD_REQUEST,
             CustomError::ReqwestClient(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            CustomError::Straico(_) => StatusCode::INTERNAL_SERVER_ERROR,
             CustomError::ResponseParse(_) => StatusCode::INTERNAL_SERVER_ERROR,
             CustomError::Anyhow(_) => StatusCode::INTERNAL_SERVER_ERROR,
         }
